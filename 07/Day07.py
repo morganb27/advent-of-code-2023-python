3210e54963737034d560fcf67e0669ef7fca210e
morganb27 <morgan.ballouk@gmail.com>
2024-07-14 07:40:22 +0200

    Day 7 part 2

--- 07/Day07.py
+++ 07/Day07.py
@@ -6,13 +6,22 @@ HANDS = []
 
 def camel_cards(data):
     sum = 0
-    for i, (hand, bid) in enumerate(sorted(data, key = lambda x: (rank(x[0]), tie_break(x[0]))), start = 1):
-        
-        print(i, hand, bid)
+    for i, (_, bid) in enumerate(sorted(data, key = lambda x: (rank(x[0]), tie_break(x[0]))), start = 1):
         sum += bid * i
     return sum
 
-def rank(hand):
+def camel_cards_part_two(data):
+    sum = 0
+    for i, (_, bid) in enumerate(sorted(data, key = lambda x: (rank(x[0], True), tie_break(x[0], True))), start = 1):
+        sum += bid * i
+    return sum
+
+def rank(hand, isPartTwo = False):
+
+    if isPartTwo and hand != 'JJJJJ':
+        no_jokers = Counter(c for c in hand if c != 'J').most_common()
+        hand = hand.replace('J', no_jokers[0][0])
+
     cmn = Counter(hand).most_common()
 
     #Five of a kind
@@ -34,10 +43,12 @@ def rank(hand):
     return 0
 
 
-def tie_break(hand):
-    return tuple(card_score(c) for c in hand)
+def tie_break(hand, isPartTwo = False):
+    return tuple(card_score(c, isPartTwo) for c in hand)
 
-def card_score(card):
+def card_score(card, isPartTwo = False):
+    if isPartTwo:
+        return 'J23456789TQKA'.index(card)
     return '23456789TJQKA'.index(card)
 
 def parse_input(data):
@@ -48,3 +59,4 @@ def parse_input(data):
 
 parse_input(PUZZLE)
 print(f"Solution for part 1 is: {camel_cards(HANDS)}")
+print(f"Solution for part 2 is: {camel_cards_part_two(HANDS)}")
