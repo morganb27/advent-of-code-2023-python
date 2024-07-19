0437f37b1652d858aa023534411f9a5c87415647
morganb27 <morgan.ballouk@gmail.com>
2024-07-19 08:25:40 +0200

    Day 9 - Part 2

--- 09/Day09.py
+++ 09/Day09.py
@@ -1,12 +1,15 @@
 import fileinput
 
 PUZZLE = [line.strip() for line in fileinput.input()]
+PART_1 = 0
+PART_2 = 0
 
 def mirage_maintenance(data):
-    total = 0
+    global PART_1, PART_2
     for line in data:
         nums = [int(num) for num in line.split()]
         diff = [nums[-1]]
+        part_one_first_values = [nums[0]]
 
         while True:
             seq = []
@@ -14,23 +17,29 @@ def mirage_maintenance(data):
                 seq.append(nums[i] - nums[i-1])
 
             diff.insert(0, seq[-1])
+            part_one_first_values.insert(0, seq[0])
 
             if all(num == 0 for num in seq):
-                total += extrapolate(diff)
+                PART_1 += extrapolate(diff)
+                PART_2 += extrapolate(part_one_first_values, True)
                 break
 
             nums = seq
-    return total
+    return PART_1
             
 
-def extrapolate(nums):
+def extrapolate(nums, part_2 = False):
     extra = [0]
+    if part_2:
+        for num in nums:
+            extra.append(num - extra[-1])
+        return extra[-1]
+    else:
+        for num in nums:
+            extra.append(extra[-1] + num)
+        return extra[-1]
 
-    for num in nums:
-        extra.append(extra[-1] + num)
-        
-    return extra[-1]
 
-
-
-print(f"Solution for part 1 is: {mirage_maintenance(PUZZLE)}")
+mirage_maintenance(PUZZLE)
+print(f"Solution for part 1 is: {PART_1}")
+print(f"Solution for part 2 is: {PART_2}")
